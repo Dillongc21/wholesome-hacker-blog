6c8f310a846a883b2b811ec1a988c7aaf3170e38
Dillongc21 <DillonGC21@gmail.com>
2021-09-29 23:41:59 -0600

    Implement blog index page
    List blog posts on index page by globbing all ".md" files in directory.
    Only implement in Health section.

---
+++ src/routes/health/index.svelte
@@ -0,0 +1,34 @@
+<script context="module">
+	const allPosts = import.meta.glob("./*.md")
+
+	let body = []
+	for(let path in allPosts) {
+		body.push(
+			allPosts[path]().then(({ metadata }) => {
+				let formattedPath = `health/${path.replace(".md","").replace("./","")}`
+				return {path:formattedPath, metadata}
+			})
+		)
+	}
+
+export const load = async() => {
+	const posts = await Promise.all(body)
+	return {
+		props: {
+			posts
+		},
+	}
+}
+</script>
+
+<script>
+	export let posts
+</script>
+
+<ul>
+	{#each posts as {path,metadata}}
+		<li>
+			<a href={path}>{metadata.title}</a>
+		</li>
+	{/each}
+</ul>
