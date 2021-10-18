949d66dc803b40b15e84826d95ece70992b7bb8a
Dillongc21 <DillonGC21@gmail.com>
2021-10-18 06:12:44 -0600

    Add nav style and fonts
    Add Roboto mono and styling to Nav Bar.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,31 @@
+<style>
+	:global(body) {
+		font-family: "Roboto Mono", Arial, sans-serif;
+	}
+	h1, nav {
+		text-align: center;
+	}
+	h1 {
+		font-weight: normal;
+	}
+	nav > a{
+		padding-right: 30px;
+		text-decoration: none;
+		font-size: 1.25rem;
+	}
+	nav > a:last-child{
+		padding-right: 0;
+	}
+	@font-face {
+		font-family: "Roboto Mono";
+		src: url("../static/fonts/RobotoMono-VariableFont_wght.ttf") format("truetype");
+		font-style: normal;
+	}
+</style>
+
+<h1>Wholesome Hacker Blog</h1>
 <nav>
 	<a href="/">Home</a>
-	<a href="/about">About</a>
 	<a href="/health">Health</a>
 	<a href="/tech">Tech</a>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,3 @@
 	<title>Wholesome Hacker Blog</title>
 </svelte:head>
 
-<h1>Wholesome Hacker Blog</h1>
